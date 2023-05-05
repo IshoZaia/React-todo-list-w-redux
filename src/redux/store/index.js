c50156c8a5328b1107d45cca86563b00c2b82f81
IshoZaia <Isho.yz.zaia@gmail.com>
2023-05-04 20:58:33 -0400

    Redux Persist

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,20 @@
-import {configureStore} from '@reduxjs/toolkit';
-import reducer from "../reducers";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({reducer})
+import rootReducer from '../reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
